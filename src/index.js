bd34813f84f22b0ef383f3d990bd5a19141e7b4d
Sogand <sogandmohamadipour7@gmail.com>
2025-07-28 18:19:22 +0330

    section 42

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 
 
@@ -50,21 +51,76 @@ const pizzaData = [
 
 
 function App() {
-  return <>
-    <h1>Hello React!!!</h1>
-    <Pizza />
-    <Pizza />
-    <Pizza />
-  </>;
+  return <div className='container'>
+    <Header />
+    <Menu />
+    <Footer />
+  </div>;
 }
 
-function Pizza() {
+
+function Header() {
   return <>
-    <img src="/pizzas/spinaci.jpg" alt="spinaci" />
-    <h2>Pizza Spinaci</h2>
-    <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <header className='header'>
+      <h1>Fast React Pizza Co.</h1>
+    </header>
   </>
 }
+
+function Menu() {
+  return (
+    <main className='menu'>
+      <h2>Our menu</h2>
+      <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName='pizzas/spinaci.jpg'
+        price={10}
+      />
+
+      <Pizza
+        name='Pizza Funghi'
+        ingredients='Tomato, mozarella, mushrooms, and onion'
+        photoName='pizzas/funghi.jpg'
+        price={12}
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div className='pizza'>
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if(hour >= openHour &&  hour <= closeHour) 
+  //   alert('we are currently open!')
+  // else 
+  //   alert('sorry we are closed')
+
+  return <>
+    <footer className='footer'>
+      {new Date().toLocaleTimeString() + '. we are currently open!'}
+    </footer>
+  </>
+}
+
+
 
 
 
