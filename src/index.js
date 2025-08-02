87e6642cf3dd4b76cb0c4dc2b1739c13352aaa5e
Sogand <sogandmohamadipour7@gmail.com>
2025-08-02 19:35:25 +0330

    end of  season 5

--- src/index.js
+++ src/index.js
@@ -112,15 +112,19 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
 
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
+
 
   return (
-    <li className='pizza'>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
+      <img src={`${process.env.PUBLIC_URL}/${pizzaObj.photoName}`} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price + 3}</span>
+
+        {/* {pizzaObj.soldOut ? <span>SOLD OUT</span> : <span>{pizzaObj.price}</span>} */}
+
+        <span>{pizzaObj.soldOut ? 'SOLD OUT' : pizzaObj.price}</span>
       </div>
     </li>
   );
